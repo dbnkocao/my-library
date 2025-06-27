d69315950ca647a03bfcce07d7c93d0443385957
Daniel Nascimento <dbnkocao@gmail.com>
2025-06-27 00:37:58 +0000

    adicionando  cep_service a engine brasil api

--- engines/brasilapi/app/services/brasilapi/base_service.rb
+++ engines/brasilapi/app/services/brasilapi/base_service.rb
@@ -1,5 +1,27 @@
+require 'rest-client'
+
 module Brasilapi
   class BaseService
     DOMAIN = 'https://brasilapi.com.br'
+
+    attr_reader :param, :response
+
+    def self.call(param:)
+      new(param: param).call
+    end
+
+    def initialize(param:)
+      @param = param
+    end
+
+    private
+
+    def request_data
+      @response = RestClient.get(url, { accept: :json })
+    end
+
+    def url
+      raise NotImplementedError, 'You must implement the url method in the subclass'
+    end
   end
 end
--- engines/brasilapi/app/services/brasilapi/isbn_service.rb
+++ engines/brasilapi/app/services/brasilapi/isbn_service.rb
@@ -1,33 +1,17 @@
-require 'rest-client'
-
 module Brasilapi
   class IsbnService < BaseService
-    attr_reader :isbn, :response
-
-    def self.call(isbn:)
-      new(isbn: isbn).call
-    end
-
-    def initialize(isbn:)
-      @isbn = isbn
-    end
-
     def call
       request_data
 
-      { success: true, response: JSON.parse(response.body) } if @response.code == 200
+      { success: true, response: JSON.parse(response.body) }
     rescue RestClient::BadRequest
-      { success: false, error: "Lirvro com o isbn #{isbn} não foi encontrado." }
+      { success: false, error: "Livro com o isbn #{param} não foi encontrado." }
     end
 
     private
 
-    def request_data
-      @response = RestClient.get(url, { accept: :json })
-    end
-
     def url
-      "#{DOMAIN}/api/isbn/v1/#{isbn}"
+      "#{DOMAIN}/api/isbn/v1/#{param}"
     end
   end
 end

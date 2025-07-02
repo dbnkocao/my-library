c9731c12ca2926273432d16cd09789e7d345ea41
Daniel Nascimento <dbnkocao@gmail.com>
2025-07-02 01:04:55 +0000

    adicionando listagem de livros

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,14 @@ class BooksController < ApplicationController
   def create
     book = Book.find_by(isbn: params[:isbn]) || ::CreateBookByIsbnService.call(params[:isbn])
 
-    current_user.library.books << book if book
+    library_books << book if book && !library_books.include?(book)
 
     redirect_to root_path
+  end
+
+  private
+
+  def library_books
+    @library_books ||= current_user.library.books
   end
 end

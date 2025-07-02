6b2f66d6e652a2fb308c258ca672d1dbce15bbf2
Daniel Nascimento <dbnkocao@gmail.com>
2025-07-02 02:33:18 +0000

    Adicionada opção de deletar livro da biblioteca

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,5 +1,23 @@
 class LibrariesController < ApplicationController
+  before_action :set_library
+
   def index
     @library = current_user.library
+  end
+
+  def delete_book
+    book = Book.find_by(id: params[:book_id])
+
+    @library.books.delete(book)
+
+    redirect_to root_path, notice: "Book '#{book.title}'deleted with success."
+  end
+
+  private
+
+  def set_library
+    @library = current_user.library
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Library not found."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   end
   resources :books, only: :create
   resources :passwords, param: :token
+
+  resources :libraries, only: :index do
+    delete "delete_book/:book_id" => "libraries#delete_book", as: :delete_book, on: :collection
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
